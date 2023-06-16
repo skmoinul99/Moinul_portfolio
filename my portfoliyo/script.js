9daa7b3cdf92b6d490da32a2f502f7a6b9f33091
SK MOINUL HAQUE <128489924+skmoinul99@users.noreply.github.com>
2023-06-17 02:26:45 +0530

    Add files via upload

---
+++ my portfoliyo/script.js
@@ -0,0 +1,16 @@
+// ------------------------------------------Scroll Section active link------------------------------------//
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top)
+
+    });
+};
